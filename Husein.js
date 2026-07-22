ff7123f9ad130e9f65e71ddc3fcb7b9458c8f5e3
Sayid Husein X RPL 6 <huseinsayid497@gmail.com>
2026-07-22 09:42:49 +0700

    Add files via upload

---
+++ Husein.js
@@ -0,0 +1,4 @@
+console.log("nama : Sayid Husein");
+console.log('Tanggal Lahir : 23 Juni 2010');
+console.log('SMP : MTsN 1 Kota Malang');
+console.log('Saya masuk ke SMK Telkom Malang karena saya ingin belajar tentang dunia IT dan teknologi yang ada disini');
---
+++ hello.js
@@ -0,0 +1,1 @@
+console.log("hello dari X RPL 6");
