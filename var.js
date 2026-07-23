da209c267a4969f383a393f8fae1587b3148010a
Sayid Husein X RPL 6 <huseinsayid497@gmail.com>
2026-07-23 08:14:01 +0700

    Add files via upload

---
+++ var.js
@@ -0,0 +1,18 @@
+//Deklarasi Variabel
+let angka = 100;        //number
+let sekolah = "Moklet"; //string
+let kondisi = false;    //boolean
+let data;               //undefined
+let nama = null;        //null
+
+console.log(typeof angka);
+console.log(typeof sekolah);
+console.log(typeof kondisi);
+console.log(typeof data);
+console.log(typeof nama);
+
+console.log(`ini adalah Angka: ${angka}`);
+console.log(`ini adalah Sekolah: ${sekolah}`);
+console.log(`ini adalah Kondisi: ${kondisi}`);
+console.log(`ini adalah Data: ${data}`);
+console.log(`ini adalah Nama: ${nama}`);
---
+++ variabel.js
@@ -0,0 +1,10 @@
+//Latihan Mandiri
+let skor = 100; //number
+let level = "Extreme"; //string
+let nyawa = true; //boolean
+const namaPemain = "Benzema"; //string
+
+console.log(`Skor: ${skor}`);
+console.log(`Level: ${level}`);
+console.log(`Nyawa: ${nyawa}`);
+console.log(`Nama Pemain: ${namaPemain}`);
