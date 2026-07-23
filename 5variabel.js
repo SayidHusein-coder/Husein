8f89cfc426bd937463f7fd4b2c70b1d53fdf16f8
Sayid Husein X RPL 6 <huseinsayid497@gmail.com>
2026-07-23 08:38:17 +0700

    Add files via upload

---
+++ 5variabel.js
@@ -0,0 +1,12 @@
+//Deklarasi 5 Variabel
+let angka = 100;        //number
+let sekolah = "Moklet"; //string
+let kondisi = false;    //boolean
+let data;               //undefined
+let nama = null;        //null  
+
+console.log(typeof angka);
+console.log(typeof sekolah);
+console.log(typeof kondisi);
+console.log(typeof data);
+console.log(typeof nama);
